6ee1d91c1cb9d43a807b93545d1573a40e594ddb
B-codes-core <bcodes2350@gmail.com>
2024-09-30 16:10:33 +0530

    Add encryption.py

---
+++ app/cryptographic_operations/encryption.py
@@ -0,0 +1,58 @@
+import secrets
+from Crypto.Cipher import AES
+
+class TagMismatchError(Exception):
+    def __init__(self, msg="Decryption Failed due to tag mismatch"):
+        super().__init__(msg)
+        self.msg = msg
+    
+    def __str__(self):
+        return self.msg
+
+def generate_key() -> bytes:
+    """
+    Generates a cryptographically secure AES-256 key
+
+    Args : None
+    Returns : A cryptographically secure AES-256 key 
+    """
+    return secrets.token_bytes(32)
+
+def encrypt_password(input_password: str, key: bytes) -> tuple:
+    """
+    Encrypts a password using AES-256 in GCM mode
+
+    Args:
+        input_password (string) : The password to be encrypted
+        key (bytes) : The AES-256 encryption key
+
+    Returns:
+        tuple : nonce, ciphertext and tag in the order meentioned
+    """
+    nonce = secrets.token_bytes(12)
+    cipher = AES.new(key, AES.MODE_GCM, nonce=nonce)
+    ciphertext, tag = cipher.encrypt_and_digest(input_password.encode('utf-8'))
+    return nonce, ciphertext, tag
+
+def decrypt_password(input_ciphertext: bytes, nonce: bytes, tag: bytes, key: bytes) -> str:
+    """
+    Decrypts a ciphertext using AES-256 GCM mode
+
+    Args:
+        input_ciphertext (bytes) : The ciphertext to be decrypted
+        nonce (bytes) : The nonce used to encrypt the input password
+        tag (bytes) : The authentication tag provided by AES-GCM during encryption
+        key (bytes) : The key that was used for encrypting the password
+
+    Returns:
+        str : The decrypted password
+
+    Raises:
+        TagMismatchhError : If tag authentication fails. This may happen if the ciphertext was tampered or input decryption key is wrong.
+    """
+    try:
+        cipher = AES.new(key, AES.MODE_GCM, nonce=nonce)
+        plaintext = cipher.decrypt_and_verify(input_ciphertext, tag)
+        return plaintext.decode('utf-8')
+    except ValueError as v:
+        raise TagMismatchError
